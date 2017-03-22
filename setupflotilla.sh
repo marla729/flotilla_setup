971cfd915e6c2f575292e5f782f2e3c1e312ef79
marla729 <carcar2014m@gmail.com>
2017-03-22 17:01:30 -0400

    Update setupflotilla.sh

--- setupflotilla.sh
+++ setupflotilla.sh
@@ -2,8 +2,8 @@
 sudo apt-get install golang
 mkdir ../flotilla
 export GOPATH=/home/x397liu/flotilla
-sudo go get github.com/v1n337/flotilla/flotilla-server
-sudo go get github.com/v1n337/flotilla/flotilla-client
+go get github.com/v1n337/flotilla/flotilla-server
+go get github.com/v1n337/flotilla/flotilla-client
 sudo apt-get install rabbitmq-server
 sudo apt-get install elixir
 sudo apt-get install docker.io
